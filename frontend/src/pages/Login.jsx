3cd669ceb5e8c5e0e881a6f8da35495863cd8dda
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-12-01 13:20:45 +0530

    Create Login.jsx

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import axios from "axios";
+import { API_BASE } from "../api";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const nav = useNavigate();
+
+  const submit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post(`${API_BASE}/api/auth/login`, { email, password });
+    localStorage.setItem("user", JSON.stringify(res.data.user));
+    nav("/");
+  };
+
+  return (
+    <div className="center">
+      <h2>Login</h2>
+      <form onSubmit={submit}>
+        <input placeholder="Email" value={email} onChange={e=>setEmail(e.target.value)} />
+        <input placeholder="Password" type="password" value={password} onChange={e=>setPassword(e.target.value)} />
+        <button>Login</button>
+      </form>
+      <p>New? <Link to="/signup">Signup</Link></p>
+    </div>
+  );
+}
