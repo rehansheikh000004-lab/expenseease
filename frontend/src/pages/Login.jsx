079e1107dd4ea57047e8eb27d6cf550eb05f3609
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-12-01 19:27:43 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,13 +7,18 @@ import { Link, useNavigate } from "react-router-dom";
 export default function Login() {
   const [email,setEmail] = useState("");
   const [password,setPassword] = useState("");
+  const [err,setErr] = useState("");
   const nav = useNavigate();
 
   const submit = async (e) => {
     e.preventDefault();
-    const res = await axios.post(`${API_BASE}/api/auth/login`, { email, password });
-    localStorage.setItem("mm_user", JSON.stringify(res.data.user));
-    nav("/");
+    try {
+      const res = await axios.post(`${API_BASE}/api/auth/login`, { email, password });
+      localStorage.setItem("ex_user", JSON.stringify(res.data.user));
+      nav("/");
+    } catch (error) {
+      setErr(error.response?.data?.message || "Error");
+    }
   };
 
   return (
@@ -21,10 +26,11 @@ export default function Login() {
       <h2>Login</h2>
       <form onSubmit={submit}>
         <input placeholder="Email" value={email} onChange={e=>setEmail(e.target.value)} />
-        <input placeholder="Password" type="password" value={password} onChange={e=>setPassword(e.target.value)} />
+        <input placeholder="Password" value={password} type="password" onChange={e=>setPassword(e.target.value)} />
         <button>Login</button>
+        <p>{err}</p>
       </form>
-      <p>New? <Link to="/signup">Signup</Link></p>
+      <Link to="/signup">Create account</Link>
     </div>
   );
 }
