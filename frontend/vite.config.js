6f2221360967ffa7bb91e3aae63b65370b069f21
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-12-01 19:21:54 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,6 @@
-export default {
-  server: { port: 3000 }
-};
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+});
